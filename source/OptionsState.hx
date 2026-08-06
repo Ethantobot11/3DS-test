365f22377f7198e18b83678cbd7ea777e7b2ee19
Ethantobot <ethantobot7@gmail.com>
2026-08-06 14:30:23 +0200

    Update OptionsState.hx

--- source/OptionsState.hx
+++ source/OptionsState.hx
@@ -53,13 +53,13 @@ class OptionsState extends CitroState {
     override public function update(delta:Int) {
         super.update(delta);
 
-        if (HID.keyPressed(HIDKey.KEY_UP) || HID.keyPressed(HIDKey.CPAD_UP)) {
+        if (HID.keyPressed(HIDKey.UP) || HID.keyPressed(HIDKey.CPAD_UP)) {
             selectedIndex--;
             if (selectedIndex < 0) selectedIndex = MAX_OPTIONS - 1;
             trace('Selected Option Index: $selectedIndex');
         }
 
-        if (HID.keyPressed(HIDKey.KEY_DOWN) || HID.keyPressed(HIDKey.CPAD_DOWN)) {
+        if (HID.keyPressed(HIDKey.DOWN) || HID.keyPressed(HIDKey.CPAD_DOWN)) {
             selectedIndex++;
             if (selectedIndex >= MAX_OPTIONS) selectedIndex = 0;
             trace('Selected Option Index: $selectedIndex');
@@ -111,7 +111,7 @@ class OptionsState extends CitroState {
     private function renderFpsCounter(delta:Int) {
         var currentFps:Int = delta > 0 ? Std.int(1000 / delta) : 60;
         
-        citro.object.CitroText.text('FPS: $currentFps', 4, 4, 0xFFFFFFFF);
+        //citro.object.CitroText.text('FPS: $currentFps', 4, 4, 0xFFFFFFFF);
     }
 
     override public function destroy() {
