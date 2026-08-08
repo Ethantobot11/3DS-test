c344634441c3d5c6598d7773fef3cf07c2a040ff
Ethantobot <ethantobot7@gmail.com>
2026-08-08 14:33:46 +0200

    Update BalancedTree.hx

--- source/haxe/ds/BalancedTree.hx
+++ source/haxe/ds/BalancedTree.hx
@@ -128,7 +128,7 @@ class BalancedTree<K, V> implements haxe.Constraints.IMap<K, V> {
 	**/
 	#if (wiiu || cafe)
 	@:runtime public inline function keyValueIterator():KeyValueIterator<K, V> {
-		return new haxe.iterators.MapKeyValueIterator(cast (this, haxe.ds.BalancedTree<K, V>));
+		return new haxe.iterators.MapKeyValueIterator(cast (this, Dynamic));
 	}
 	#elseif (haxe3ds || nx)
 	@:runtime public inline function keyValueIterator():KeyValueIterator<K, V> {
