a74b73670deef429c815e949e6f126be93b2ee18
Ethantobot <ethantobot7@gmail.com>
2026-08-08 17:35:50 +0200

    Update CallStack.hx

--- source/haxe/CallStack.hx
+++ source/haxe/CallStack.hx
@@ -36,6 +36,7 @@ namespace haxe {
 @:headerInclude("deque", true)
 @:headerInclude("memory", true)
 @:headerInclude("haxe_NativeStackTrace.h", true)
+#end
 enum StackItem {
 	CFunction;
 	Module(m:String);
