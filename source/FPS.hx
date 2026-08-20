195eb01b06f7b2e75f47b63de3b3add6eeea5e04
Ethantobot <ethantobot7@gmail.com>
2026-08-20 21:45:18 +0200

    Create FPS.hx

---
+++ source/FPS.hx
@@ -0,0 +1,33 @@
+package;
+
+#if haxe3ds
+import citro.object.CitroText;
+#else
+import leafy.LfEngine;
+import leafy.backend.sdl.LfWindowRender;
+#end
+
+class FPS
+{
+    public var fpsText:CitroText;
+
+    public function showfpsbro():Void
+    {
+        fpsText = new CitroText(2, 2, "FPS: 60");
+        fpsText.color = 0xFFFFFF00;
+        add(fpsText);
+    }
+        
+    public function main():Void
+    {
+        #if haxe3ds
+        
+        trace("Starting FPS Counter for 3DS Application...");
+
+        showfpsbro();
+        
+        #else
+        LfEngine.initEngine("Deltarune", DRC, new WiiUMainMenuState());
+        #end
+    }
+}
