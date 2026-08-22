abf1da5c1508a2bd2c71d389db69e9593bbdb8bf
Ethantobot <ethantobot7@gmail.com>
2026-08-23 00:57:45 +0200

    Add drawBottom parameter to drawing functions

--- source/citro/c2d/CitroObjectDraw.hx
+++ source/citro/c2d/CitroObjectDraw.hx
@@ -8,6 +8,8 @@ import citro.backend.CitroColor;
 @:headerCode('
 #include <citro2d.h>
 #include <citro3d.h>
+extern C3D_RenderTarget* topScreen;
+extern C3D_RenderTarget* bottomScreen;
 ')
 
 @:cppFileCode('
@@ -27,58 +29,61 @@ static inline u32 colorConvert(int color) {
 
 class CitroObjectDraw {
 
-	public static function drawTriangle(arrayX:Array<Float>, arrayY:Array<Float>, arrayC:Array<CitroColor>):Bool {
+	public static function drawTriangle(arrayX:Array<Float>, arrayY:Array<Float>, arrayC:Array<CitroColor>, drawBottom:Bool = false):Bool {
 		if (arrayX.length > 2 && arrayY.length > 2 && arrayC.length > 0) {
 			while (arrayC.length < 3) {
 				arrayC.push(arrayC[0]);
 			}
 
 			untyped __cpp__('
+				C2D_SceneBegin(drawBottom ? bottomScreen : topScreen);
 				C2D_DrawTriangle(
 					arrayX->__get(0), arrayY->__get(0), colorConvert(arrayC->__get(0)),
 					arrayX->__get(1), arrayY->__get(1), colorConvert(arrayC->__get(1)),
 					arrayX->__get(2), arrayY->__get(2), colorConvert(arrayC->__get(2)),
 					1
 				)
-			');
+			', drawBottom);
 			return true;
 		}
 		return false;
 	}
 
-	public static function drawLine(arrayX:Array<Float>, arrayY:Array<Float>, arrayC:Array<CitroColor>, thickness:Float = 4):Bool {
+	public static function drawLine(arrayX:Array<Float>, arrayY:Array<Float>, arrayC:Array<CitroColor>, thickness:Float = 4, drawBottom:Bool = false):Bool {
 		if (arrayX.length > 1 && arrayY.length > 1 && arrayC.length > 1) {
 			if (arrayC.length == 1) {
 				arrayC.push(arrayC[0]);
 			}
 
 			untyped __cpp__('
+				C2D_SceneBegin(drawBottom ? bottomScreen : topScreen);
 				C2D_DrawLine(
 					arrayX->__get(0), arrayY->__get(0), colorConvert(arrayC->__get(0)),
 					arrayX->__get(1), arrayY->__get(1), colorConvert(arrayC->__get(1)),
 					thickness, 1
 				)
-			');
+			', drawBottom);
 			return true;
 		}
 		return false;
 	}
 
-	public static function drawRect(x:Float, y:Float, w:Float, h:Float, color:Array<CitroColor>):Bool {
+	public static function drawRect(x:Float, y:Float, w:Float, h:Float, color:Array<CitroColor>, drawBottom:Bool = false):Bool {
 		return color.length > 1 ? {
 			while (color.length < 4)
 				color.push(color[0]);
 			untyped __cpp__('
+				C2D_SceneBegin(drawBottom ? bottomScreen : topScreen);
 				C2D_DrawRectangle(
 					x, y, 1, w, h, colorConvert(color->__get(0)), colorConvert(color->__get(1)),
 					colorConvert(color->__get(2)), colorConvert(color->__get(3))
 				)
-			');
+			', drawBottom);
 			true;
 		} : false;
 	}
 	
-	public static function drawCircle(x:Float, y:Float, radius:Float, color:Array<CitroColor>):Bool {
+	public static function drawCircle(x:Float, y:Float, radius:Float, color:Array<CitroColor>, drawBottom:Bool = false):Bool {
 		if (color.length == 0)
 			return false;
 		
@@ -86,12 +91,13 @@ class CitroObjectDraw {
 			color.push(color[0]);
 
 		untyped __cpp__('
+			C2D_SceneBegin(drawBottom ? bottomScreen : topScreen);
 			C2D_DrawCircle(x, y, 1, radius, colorConvert(color->__get(0)), colorConvert(color->__get(1)), colorConvert(color->__get(2)), colorConvert(color->__get(3)))
-		');
+		', drawBottom);
 		return true;
 	}
 
-	public static function drawEllipse(x:Float, y:Float, w:Float, h:Float, color:Array<CitroColor>):Bool {
+	public static function drawEllipse(x:Float, y:Float, w:Float, h:Float, color:Array<CitroColor>, drawBottom:Bool = false):Bool {
 		if (color.length == 0)
 			return false;
 		
@@ -99,8 +105,9 @@ class CitroObjectDraw {
 			color.push(color[0]);
 	
 		untyped __cpp__('
+			C2D_SceneBegin(drawBottom ? bottomScreen : topScreen);
 			C2D_DrawEllipse(x, y, 1, w, y, colorConvert(color->__get(0)), colorConvert(color->__get(1)), colorConvert(color->__get(2)), colorConvert(color->__get(3)))
-		');
+		', drawBottom);
 		return true;
 	}
 }
