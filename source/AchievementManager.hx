ba6acce2353486712318a50f92d8eceaa98a5c98
Ethantobot <ethantobot7@gmail.com>
2026-09-18 22:25:06 +0200

    Create AchievementManager.hx

---
+++ source/AchievementManager.hx
@@ -0,0 +1,16 @@
+class AchievementManager {
+    public static function unlock(achievementID:String) {
+        if (CitroG.save.data.achievements == null) {
+            CitroG.save.data.achievements = {};
+        }
+
+        var currentStatus = Reflect.field(CitroG.save.data.achievements, achievementID);
+        if (currentStatus != true) {
+            Reflect.setField(CitroG.save.data.achievements, achievementID, true);
+            CitroG.save.flush();
+            
+            trace('Achievement Unlocked: $achievementID');
+            SoundPlayer.playSound('romfs:/assets/sounds/snd_save.cwav');
+        }
+    }
+}
