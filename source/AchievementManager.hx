1ecdd78548bcf0c2bdc6475edb3300a9c2dda22d
Ethantobot <ethantobot7@gmail.com>
2026-09-18 22:27:00 +0200

    Refactor unlock function to include title and description
    Updated the unlock function to accept additional parameters for title, description, and icon path, and modified the achievement unlocking logic accordingly.

--- source/AchievementManager.hx
+++ source/AchievementManager.hx
@@ -2,18 +2,17 @@ import citro.CitroG;
 import citro.CitroSave;
 
 class AchievementManager {
-    public static function unlock(achievementID:String) {
+    public static function unlock(id:String, title:String, desc:String, iconPath:String) {
         if (CitroG.save.data.achievements == null) {
             CitroG.save.data.achievements = {};
         }
-
-        var currentStatus = Reflect.field(CitroG.save.data.achievements, achievementID);
+    
+        var currentStatus = Reflect.field(CitroG.save.data.achievements, id);
         if (currentStatus != true) {
-            Reflect.setField(CitroG.save.data.achievements, achievementID, true);
+            Reflect.setField(CitroG.save.data.achievements, id, true);
             CitroG.save.flush();
-            
-            trace('Achievement Unlocked: $achievementID');
-            SoundPlayer.playSound('romfs:/assets/sounds/snd_save.cwav');
+    
+            AchievementPopup.show(title, desc, iconPath);
         }
     }
 }
